9bc0c77d083692884fefdbb8f5eca734e6f308b7
ChimpGamer <Tim.Cremers@Ziggo.nl>
2024-11-25 21:45:02 +0100

    Added force disguise flag

--- src/main/kotlin/nl/chimpgamer/libsdisguiseswgflags/LibsDisguisesWGFlagsPlugin.kt
+++ src/main/kotlin/nl/chimpgamer/libsdisguiseswgflags/LibsDisguisesWGFlagsPlugin.kt
@@ -3,11 +3,14 @@ package nl.chimpgamer.libsdisguiseswgflags
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldguard.WorldGuard
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin
+import com.sk89q.worldguard.protection.flags.Flag
 import com.sk89q.worldguard.protection.flags.StateFlag
+import com.sk89q.worldguard.protection.flags.StringFlag
 import com.sk89q.worldguard.protection.flags.registry.FlagConflictException
 import com.sk89q.worldguard.session.SessionManager
 import nl.chimpgamer.libsdisguiseswgflags.listeners.LibsDisguiseListener
 import nl.chimpgamer.libsdisguiseswgflags.handlers.BlockDisguisesHandler
+import nl.chimpgamer.libsdisguiseswgflags.handlers.ForceDisguiseHandler
 import org.bukkit.Location
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
@@ -15,42 +18,48 @@ import java.util.logging.Level
 
 class LibsDisguisesWGFlagsPlugin : JavaPlugin() {
     val blockDisguisesFlag = StateFlag("block-disguises", true) // Default is allowed
+    val forceDisguiseFlag = StringFlag("force-disguise")
 
     private val blockDisguisesHandler = BlockDisguisesHandler.createFactory()
+    private val forceDisguiseHandler = ForceDisguiseHandler.createFactory()
 
     private val sessionManager: SessionManager get() = WorldGuard.getInstance().platform.sessionManager
 
     override fun onLoad() {
         instance = this
         registerFlag(blockDisguisesFlag)
+        registerFlag(forceDisguiseFlag)
     }
 
     override fun onEnable() {
-        sessionManager.registerHandler(blockDisguisesHandler, null)
+        sessionManager.run {
+            registerHandler(blockDisguisesHandler, null)
+            registerHandler(forceDisguiseHandler, null)
+        }
         server.pluginManager.registerEvents(LibsDisguiseListener(this), this)
     }
 
     override fun onDisable() {
-        sessionManager.unregisterHandler(blockDisguisesHandler)
+        sessionManager.run {
+            unregisterHandler(blockDisguisesHandler)
+            unregisterHandler(forceDisguiseHandler)
+        }
     }
 
     fun testFlag(player: Player, location: Location, stateFlag: StateFlag): Boolean {
         val localPlayer = WorldGuardPlugin.inst().wrapPlayer(player)
         val regionContainer = WorldGuard.getInstance().platform.regionContainer
         val regionQuery = regionContainer.createQuery()
-        return sessionManager.hasBypass(localPlayer, localPlayer.world) || regionQuery.testState(
-            BukkitAdapter.adapt(
-                location
-            ), localPlayer, stateFlag
-        )
+        return sessionManager.hasBypass(localPlayer, localPlayer.world) ||
+                regionQuery.testState(BukkitAdapter.adapt(location), localPlayer, stateFlag)
     }
 
-    fun registerFlag(stateFlag: StateFlag) {
+    fun registerFlag(flag: Flag<*>) {
         val flagRegistry = WorldGuard.getInstance().flagRegistry
         try {
-            flagRegistry.register(stateFlag)
+            flagRegistry.register(flag)
         } catch (ex: FlagConflictException) {
-            logger.log(Level.SEVERE, "Unable to register flag ${stateFlag.name}", ex)
+            logger.log(Level.SEVERE, "Unable to register flag ${flag.name}", ex)
         }
     }
 
---
+++ src/main/kotlin/nl/chimpgamer/libsdisguiseswgflags/handlers/ForceDisguiseHandler.kt
@@ -0,0 +1,93 @@
+package nl.chimpgamer.libsdisguiseswgflags.handlers
+
+import com.sk89q.worldedit.util.Location
+import com.sk89q.worldedit.world.World
+import com.sk89q.worldguard.LocalPlayer
+import com.sk89q.worldguard.bukkit.BukkitPlayer
+import com.sk89q.worldguard.protection.ApplicableRegionSet
+import com.sk89q.worldguard.session.MoveType
+import com.sk89q.worldguard.session.Session
+import com.sk89q.worldguard.session.handler.FlagValueChangeHandler
+import com.sk89q.worldguard.session.handler.Handler
+import me.libraryaddict.disguise.DisguiseAPI
+import me.libraryaddict.disguise.disguisetypes.Disguise
+import me.libraryaddict.disguise.utilities.parser.DisguiseParser
+import nl.chimpgamer.libsdisguiseswgflags.LibsDisguisesWGFlagsPlugin
+
+class ForceDisguiseHandler(session: Session?) :
+    FlagValueChangeHandler<String?>(session, LibsDisguisesWGFlagsPlugin.instance.forceDisguiseFlag) {
+
+    companion object {
+        fun createFactory(): Handler.Factory<*> = Factory()
+    }
+
+    class Factory : Handler.Factory<ForceDisguiseHandler>() {
+        override fun create(session: Session?): ForceDisguiseHandler {
+            return ForceDisguiseHandler(session)
+        }
+    }
+
+    var originalDisguise: Disguise? = null
+
+    override fun onInitialValue(player: LocalPlayer, set: ApplicableRegionSet?, value: String?) {
+        handleValue(player, player.world, value)
+    }
+
+    override fun onSetValue(
+        player: LocalPlayer,
+        from: Location?,
+        to: Location,
+        toSet: ApplicableRegionSet?,
+        currentValue: String?,
+        lastValue: String?,
+        moveType: MoveType?
+    ): Boolean {
+        handleValue(player, to.extent as World, currentValue, lastValue)
+        return true
+    }
+
+    override fun onAbsentValue(
+        player: LocalPlayer,
+        from: Location?,
+        to: Location,
+        toSet: ApplicableRegionSet?,
+        lastValue: String?,
+        moveType: MoveType?
+    ): Boolean {
+        handleValue(player, to.extent as World, null, lastValue)
+        return true
+    }
+
+    private fun handleValue(localPlayer: LocalPlayer, world: World, value: String?, lastValue: String? = null) {
+        val bukkitPlayer = (localPlayer as BukkitPlayer).player
+
+        if (!this.session.manager.hasBypass(localPlayer, world)) {
+            if (value != null && lastValue == null) {
+                originalDisguise = if (DisguiseAPI.isDisguised(bukkitPlayer)) {
+                    DisguiseAPI.getDisguise(bukkitPlayer)
+                } else {
+                    null
+                }
+            }
+            if (value != null) {
+                val newDisguise = runCatching { DisguiseParser.parseDisguise(value) }.getOrNull()
+                if (DisguiseAPI.getDisguises(bukkitPlayer) == newDisguise) return
+                if (newDisguise != null) {
+                    DisguiseAPI.disguiseToAll(bukkitPlayer, newDisguise)
+                }
+            } else {
+                if (originalDisguise != null) {
+                    DisguiseAPI.disguiseToAll(bukkitPlayer, originalDisguise)
+                } else {
+                    DisguiseAPI.undisguiseToAll(bukkitPlayer)
+                }
+            }
+        } else {
+            if (originalDisguise != null) {
+                DisguiseAPI.disguiseToAll(bukkitPlayer, originalDisguise)
+            } else {
+                DisguiseAPI.undisguiseToAll(bukkitPlayer)
+            }
+        }
+    }
+}
